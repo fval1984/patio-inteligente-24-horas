01f53e2bdb5c38b03d90b900f29633a53c3c5cc4
fval1984 <patio@ampliauto.com.br>
2026-05-11 13:06:08 -0300

    Create jwt.strategy.ts

---
+++ services/banking-api/src/modules/auth/jwt.strategy.ts
@@ -0,0 +1,21 @@
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+
+export type JwtPayload = { sub: string };
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(config: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: config.get<string>("jwt.secret"),
+    });
+  }
+
+  validate(payload: JwtPayload) {
+    return { sub: payload.sub };
+  }
+}
