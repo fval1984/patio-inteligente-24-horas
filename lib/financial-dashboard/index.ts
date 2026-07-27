77355cea7f35ea07454d62c0d9e51e6a98cd229c
fval1984 <patio@ampliauto.com.br>
2026-07-27 09:16:36 -0300

    Create index.ts

---
+++ lib/financial-dashboard/index.ts
@@ -0,0 +1,14 @@
+export type {
+  FinancialFilters,
+  FinancialMetricsResult,
+  FinancialKpis,
+  FinancialAlerts,
+  FinancialDataSnapshot,
+  FinancialPeriodKey,
+  FinancialStatusFilter,
+} from "./types";
+
+export { DEFAULT_FINANCIAL_FILTERS } from "./types";
+export { FinancialRepository } from "./repository";
+export { FinancialMetricsService, toLocalYmd, todayYmd } from "./metrics";
+export { FinancialDashboardService, financialDashboardService } from "./service";
